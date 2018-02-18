5c373cf70c22879255dbd425c70cfc254fc44fd5
Nathenael Tarekegn <tarekegnnathenael@gmail.com>
2018-02-17 23:48:37 -0800

    Add project files.

---
+++ BHSM/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BHSM.Startup))]
+namespace BHSM
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
