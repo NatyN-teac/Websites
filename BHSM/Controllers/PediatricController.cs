6eec6c4f3d5247f47aacdecd7496ae6adf556a03
Naty <tarekegnnathenael@gmail.com>
2018-03-29 00:27:13 -0700

    major department and some minor UI

---
+++ BHSM/Controllers/PediatricController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BHSM.Controllers
+{
+    public class PediatricController : Controller
+    {
+        // GET: Pediatric
+        public ActionResult Home()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult History()
+        {
+            return View();
+        }
+        public ActionResult Unit()
+        {
+            return View();
+        }
+        public ActionResult staff()
+        {
+            return View();
+        }
+        public ActionResult research()
+        {
+            return View();
+        }
+        public ActionResult program()
+        {
+            return View();
+        }
+        public ActionResult contact()
+        {
+            return View();
+        }
+        public ActionResult calendar()
+        {
+            return View();
+        }
+    }
+}
