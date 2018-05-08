5f5cbdf24c47c12341c2050516331472c414081d
NatyBute <tarekegnnathenael@gmail.com>
2018-05-08 11:39:32 -0700

    implement saving a gig

--- GigHub/Models/Gig.cs
+++ GigHub/Models/Gig.cs
@@ -11,8 +11,12 @@ namespace GigHub.Models
 
         public int Id { get; set; }
 
-        [Required]
+        
         public ApplicationUser Artist { get; set; }
+
+        [Required]
+        public string ArtistId { get; set; }
+
 
         public DateTime DateTime { get; set; }
 
@@ -21,5 +25,9 @@ namespace GigHub.Models
         public string Venue { get; set; }
 
         public Genre Genre { get; set; }
+
+        [Required]
+        public int GenreId { get; set; }
+
     }
 }
