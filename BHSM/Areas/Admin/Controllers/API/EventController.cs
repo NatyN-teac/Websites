0128b92a877f39ad94992b20d43a49e1e9a8d14b
Nathenael <tarekegnnathenael@gmail.com>
2018-09-16 06:10:01 -0700

    re-summurizing from page one

---
+++ BHSM/Areas/Admin/Controllers/API/EventController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BHSM.Areas.Admin.ViewModel;
+
+namespace BHSM.Areas.Admin.Controllers.API
+{
+    public class EventController : ApiController
+    {
+    public IHttpActionResult CreateEvent(EventViewModel vm) {
+
+
+            return Ok();
+    }
+
+    }
+}
--- BHSM/Areas/Admin/Controllers/EventController.cs
+++ BHSM/Areas/Admin/Controllers/EventController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BHSM.Areas.Admin.ViewModel;
 
 namespace BHSM.Areas.Admin.Controllers
 {
@@ -14,7 +15,10 @@ namespace BHSM.Areas.Admin.Controllers
             return View();
         }
 
-        public ActionResult CreateEvent() {
+        [HttpPost]
+        public ActionResult Create() {
+
+
 
             return View();
         }
