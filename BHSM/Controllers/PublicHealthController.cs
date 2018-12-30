526fe6c94745e5c988aefce1dad29d53a5a61e89
Nathenael <tarekegnnathenael@gmail.com>
2018-12-30 05:11:45 -0800

    chs home page without organogram

---
+++ BHSM/Controllers/PublicHealthController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BHSM.Controllers
+{
+    public class PublicHealthController : Controller
+    {
+        // GET: PublicHealth
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
